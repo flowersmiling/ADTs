7dd95d072ed39d24cd07ceb001f8c09d6718d53e
flowersmiling <lcl8885@gmail.com>
2022-02-28 18:43:45 -0700

    create stack ADT

--- src/sait/parser/contracts/StackADT.java
+++ src/sait/parser/contracts/StackADT.java
@@ -1,13 +1,43 @@
 package sait.parser.contracts;
 
+/**
+ * Class Definition: This interface represents the public contract for the Stack Abstract Data Type. 
+ */
 public interface StackADT<E> 
-{
-	//public void stack();
+{	
 	
+	/** 
+	* Add an element to the top of a stack
+	* 
+	* Precondition:
+	* 
+	* Postcondition:
+	* 
+	* @param e
+	* @return
+	*/	
 	public boolean push(E e);
 	
+	/** 
+	* Remove the topmost element from a stack 
+	* 
+	* Precondition:
+	* 
+	* Postcondition:
+	* 
+	* @return
+	*/	
 	public E pop();
 	
+	/** 
+	* Retrieve the topmost element in a stack without removing it
+	* 
+	* Precondition:
+	* 
+	* Postcondition:
+	* 
+	* @return
+	*/	
 	public E peek();
 	
 }
