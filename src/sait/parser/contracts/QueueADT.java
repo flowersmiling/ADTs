3b14cc2acaddca8ad02d30266b46c960f938f5d4
CapBarbosssa <97461923+CapBarbosssa@users.noreply.github.com>
2022-03-01 20:52:52 -0700

    Update QueueADT.java
    adding queue methods

--- src/sait/parser/contracts/QueueADT.java
+++ src/sait/parser/contracts/QueueADT.java
@@ -1,7 +1,42 @@
 package sait.parser.contracts;
 
-public interface QueueADT {
+public interface QueueADT <E>{
 	
+	
+	public void enqueue (E e);
+	// Removes and returns the element at the front of the queue
+    //Precondition:
+	 
+	//Postcondition:
+	
+	
+	public E dequeue();
+	// Returns without removing the element at the front of the queue
+	//Precondition:
+	 
+	//Postcondition:
+	
+	
+	public E first();
+	// Returns true if the queue contains no elements
+	//Precondition:
+	 
+	//Postcondition:
+	
+	public boolean isEmpty();
+	// Returns the number of elements in the queue
+	//Precondition:
+	 
+	//Postcondition:
+	
+	public int size();
+	// Returns a string representation of the queue
+	//Precondition:
+	 
+	//Postcondition:
+	
+	
+	public String toString();
 
 }
 	
