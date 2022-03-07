9b254dec449fe5749a7f72af2f141fbf16a4b0f7
CapBarbosssa <97461923+CapBarbosssa@users.noreply.github.com>
2022-03-06 22:51:40 -0700

    Update MyArrayList.java
    adding implementations in MyArrayList

--- src/sait/parser/utility/MyArrayList.java
+++ src/sait/parser/utility/MyArrayList.java
@@ -3,6 +3,9 @@
  */
 package sait.parser.utility;
 
+import java.lang.reflect.Array;
+import java.util.NoSuchElementException;
+
 import sait.parser.contracts.Iterator;
 import sait.parser.contracts.ListADT;
 
@@ -13,55 +16,107 @@ import sait.parser.contracts.ListADT;
  */
 public class MyArrayList<E> implements ListADT<E> {
 	
+	//attributes
 	private static final long serialVersionUID =1L;
-	//attribute
+	private E[] array;
+	//capacity of a array
+	private int size;
+	private MyArrayList<E>.ArrayBasedIterator myIter;
 	
+	
+	
+	public void MyArrayList() {
+		array = (E[]) new Object [10];
+		this.size =0;
+		this.myIter = new ArrayBasedIterator();
+		
+		
+		
+	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return this.size;
 	}
 
 	@Override
 	public void clear() {
-		// TODO Auto-generated method stub
+		this.size =0;
 		
 	}
 
 	@Override
 	public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
+		if (size == this.array.length) {
+		//array has no room,create a new array with double the size
+		System.arraycopy(this.array, 0,this.array, index+1, (int) Math.pow(this.array.length, 2));
+		
+	} // if the index of a array is empty ,add the element.
+		if(this.array[index] == null ) {
+			this.array[index] = toAdd;
+			size++;
+			return true;
+			
+		}
+		// if the index of a array is not empty, move elements to the right.
+		if(this.array[index] != null) {
+			for(int i = size -2; i >= index; i--) {
+				this.array[i] = this.array[i-1];
+				size++;
+				
+			}
+		}
 		return false;
+		
 	}
 
 	@Override
 	public boolean add(E toAdd) throws NullPointerException {
-		// TODO Auto-generated method stub
+		if( toAdd ==null) {
+		return false;
+		}
+		for(int i = 0; i<this.array.length;i++) {
+			if(this.array[i] == null) {
+				this.array[i] =toAdd;
+				return true;
+			}
+			
+		}
 		return false;
 	}
 
 	@Override
 	public boolean addAll(ListADT<? extends E> toAdd) throws NullPointerException {
-		// TODO Auto-generated method stub
-		return false;
+		Iterator iterator = toAdd.iterator();
+		boolean success = true;
+		
+		while (success && iterator.hasNext()) {
+		Object next = iterator.next();
+		success = this.add((E) next);
+		}
+		return success;
 	}
 
 	@Override
 	public E get(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return (E)this.array[index];
 	}
 
 	@Override
 	public E remove(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
+		Object arr = this.array[index];
+		
+		if(index < array.length -1) {
+			System.arraycopy(this.array, index-1, this.array, index, this.array.length - index -1);
+		}
 		return null;
 	}
 
 	@Override
 	public E remove(E toRemove) throws NullPointerException {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
@@ -97,8 +152,33 @@ public class MyArrayList<E> implements ListADT<E> {
 
 	@Override
 	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		this.myIter = new ArrayBasedIterator();
+		return myIter;
+	}
+	
+	/*---------------------------------INNER CLASS------------------*/
+	 private class ArrayBasedIterator implements Iterator<E> {
+	 private int position = 0;
+	private E[] copyOfElements;
+	 private ArrayBasedIterator() {
+	copyOfElements = (E[]) toArray();
+	}
+	 @Override
+	public boolean hasNext() {
+	if (position < copyOfElements.length) {
+	return true;
+	}
+	else {
+	return false;
+	}
+	}
+	 @Override
+	public E next() throws NoSuchElementException {
+	E toReturn = copyOfElements[position];
+	position++;
+	return toReturn;
 	}
 
+}
 }
