687e2dc87404c13a8b5a87306fba7cb7c9fed62c
flowersmiling <lcl8885@gmail.com>
2022-03-24 13:16:33 -0600

    Add MyQueue and MyQueueTests

--- src/sait/parser/utility/MyQueue.java
+++ src/sait/parser/utility/MyQueue.java
@@ -1,7 +1,6 @@
-/**
- * 
- */
 package sait.parser.utility;
+
+import java.util.NoSuchElementException;
 
 import sait.parser.contracts.EmptyQueueException;
 import sait.parser.contracts.Iterator;
@@ -9,100 +8,146 @@ import sait.parser.contracts.QueueADT;
 
 /**
  * @author Chaoling Lu,Sheng Ming Yan,Mengdan Liu
- * @param <E>
+ * @param <E> - Element
  *
  */
-public class MyQueue<E> implements QueueADT<E> {
+public class MyQueue<E> implements QueueADT<E>,Iterator<E> {
 	
+	/**
+	 *The version number for Queue class  
+	 */
+	private static final long serialVersionUID = 1L;
+	private MyDLL<E> dLinkedList;
 	private int size;
-	private int head;
-	private int tail;
-	private E[] array;
-	private int capacity;
+	private int maxsize;
 	
+	/**
+	 * MyQueue data model constructor
+	 */
+	public MyQueue() 
+	{
+		this.dLinkedList = new MyDLL<E>();
+	}
 	
-	public MyQueue( int maxSize) {
-		
-		this.capacity = maxSize;
-		this.head = -1;
-		this.tail = -1;	
-		array = (E[]) new Object [this.capacity];
+	/**
+	 * MyQueue data model constructor with capacity
+	 * @param maxsize - MyQueue's capacity
+	 */
+	public MyQueue(int maxsize) 
+	{
+		this.dLinkedList = new MyDLL<E>();
+		this.maxsize = maxsize;
 	}
 
+	@Override
+	public void enqueue(E toAdd) throws NullPointerException 
+	{
+		if( toAdd == null) { throw new NullPointerException(); }
+		if( this.dLinkedList.add(toAdd) )
+			size++;
+	}
 
 	@Override
-	public void enqueue(E toAdd) throws NullPointerException {
-		
-		if( isFull() ) {
-			
-			throw new IndexOutOfBoundsException ("queue is full");
-			
+	public E dequeue() throws EmptyQueueException 
+	{
+		if( this.dLinkedList.isEmpty() ) 
+		{ 
+			return null; 
+		}else {
+			size--;
+			return this.dLinkedList.remove(0);
 		}
-		else {
-			tail++;
-			
-			array[tail] = toAdd;
-	}
-	}
-	@Override
-	public E dequeue() throws EmptyQueueException {
-		if(isEmpty()) {
-			throw new EmptyQueueException("Queue is Empty");
-		}
-		
-		head++;
-		return array[head];
 		
 	}
 	
 	@Override
-	public boolean isFull() throws IndexOutOfBoundsException{
-		
-		if(size == capacity) {
-		return true;
-	}
-		else
+	public boolean isFull() throws IndexOutOfBoundsException
+	{
+		if( this.size > this.maxsize )
+		{
+			throw new IndexOutOfBoundsException();
+		}else if( this.size == this.maxsize ) {
+			return true;
+		}else {
 			return false;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		
-		if(head == tail) {
-		return true;
-	}
-		else
-			return false;
-	}
-
-	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public E get(int index) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public E peek() throws EmptyQueueException {
-		if(isEmpty()) {
-			throw new EmptyQueueException("Queue is Empty");
 		}
-		else
-		return array[head+1];
 	}
 
+	@Override
+	public boolean isEmpty() 
+	{
+		if( this.dLinkedList.isEmpty() )
+			return true;
+		return false;
+	}
+
+	@Override
+	public Iterator<E> iterator() 
+	{
+		return this.dLinkedList.iterator();
+	}
 	
+	@Override
+	public int size() 
+	{
+		return this.size;
+	}
+
+	@Override
+	public E peek() throws EmptyQueueException 
+	{
+		return this.dLinkedList.get(0);
+	}
+
+
+	@Override
+	public void dequeueAll() 
+	{
+		this.dLinkedList.clear();
+		this.size = 0;
+	}
+
+
+	@Override
+	public boolean equals(QueueADT<E> that) 
+	{
+		boolean isEqual = true;
+		if(that.size() != this.size)
+			return false;
+		for( int i = 0; i < this.size; i++) 
+		{
+			if( !this.dLinkedList.toArray()[i].equals(that.toArray()[i]))
+				isEqual = false;
+		}
+		return isEqual;
+	}
+
+
+	@Override
+	public Object[] toArray() 
+	{
+		return this.dLinkedList.toArray();
+	}
+
+
+	@Override
+	public E[] toArray(E[] holder) throws NullPointerException 
+	{
+		return this.dLinkedList.toArray(holder);
+	}
+
+
+	@Override
+	public boolean hasNext() 
+	{
+		return this.dLinkedList.hasNext();
+	}
+
+
+	@Override
+	public E next() throws NoSuchElementException 
+	{
+		return this.dLinkedList.next();
+	}
+
 }
